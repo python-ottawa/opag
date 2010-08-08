1468fe48ddbb1eadb16ab54082d756eedcc55219
Michael P. Soulier <msoulier@digitaltorque.ca>
2010-08-08 18:53:40 -0400

    Admin site working without inlines

--- main/admin.py
+++ main/admin.py
@@ -5,16 +5,14 @@ class NoticeAdmin(admin.ModelAdmin):
     list_display = ('title', 'visible')
     list_display_links = ('title',)
 
-class MeetingTalkInline(admin.StackedInline):
-    model = MeetingTalk
+class MeetingTalkAdmin(admin.ModelAdmin):
+    pass
 
 class MeetingAdmin(admin.ModelAdmin):
-    model = Meeting
-    inlines = [ MeetingTalkInline ]
     list_display = ('date', 'firm', 'speakers_wanted')
     list_display_links = ('date',)
     list_filter = ('firm',)
 
 admin.site.register(Notice, NoticeAdmin)
 admin.site.register(Meeting, MeetingAdmin)
-admin.site.register(MeetingTalk, MeetingTalkInline)
+admin.site.register(MeetingTalk, MeetingTalkAdmin)
