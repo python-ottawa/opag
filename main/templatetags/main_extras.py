24c3f13d811267c0f7fa0ef288f2968eb44071ab
Michael P. Soulier <msoulier@digitaltorque.ca>
2010-08-22 15:09:38 -0400

    Refactoring meetings into an inclusion tag.

--- main/templatetags/main_extras.py
+++ main/templatetags/main_extras.py
@@ -2,3 +2,11 @@ from django import template
 from django.shortcuts import get_object_or_404
 
 register = template.Library()
+
+@register.inclusion_tag('main/meeting.html', takes_context=True)
+def render_meeting(context, meeting, short=False):
+    """The inclusion tag for rendering a meeting."""
+    return {
+        'meeting': meeting,
+        'short': short
+        }
