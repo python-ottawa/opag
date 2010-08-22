50e5cd786369fdb7ab2ac32828cafbab7e43a921
Michael P. Soulier <msoulier@digitaltorque.ca>
2010-08-22 10:25:49 -0400

    Added a paginator for the past_meetings page.

--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import Http404
+from django.core.paginator import Paginator
 from opag.main.models import Meeting
 from opag.lib.shared import make_bctrail
 from opag.lib.paginator import OpagPaginator
@@ -33,13 +34,18 @@ def membership(request):
                               { 'bctrail': bctrail },
                                 context_instance=RequestContext(request))
 
-def past_meetings(request):
+def past_meetings(request, pagenum=1):
     "The archived meetings page."
+    nperpage = 2
     bctrail = make_bctrail(['Home', '/',
                             'Past meetings'])
     meetings = Meeting.objects.all().filter(date__lt=date.today()).order_by('-date')
+    paginator = Paginator(meetings, nperpage)
+    page = paginator.page(pagenum)
+    page.pagenum = pagenum
     return render_to_response('main/past_meetings.html',
-                              { 'meetings': meetings,
+                              { 'meetings': page.object_list,
+                                'page': page,
                                 'bctrail': bctrail },
                                 context_instance=RequestContext(request))
 
